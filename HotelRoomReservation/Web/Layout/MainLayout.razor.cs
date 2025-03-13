55fb333fdb0d999d2042b0d6edfa177d8f69584c
Акимов Максим <105425019+MsiGrib@users.noreply.github.com>
2025-03-13 21:16:19 +0300

    йцук

---
+++ HotelRoomReservation/Web/Layout/MainLayout.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Web.Layout
+{
+    public partial class MainLayout : LayoutComponentBase
+    {
+        [Inject] 
+        public required NavigationManager Navigation { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Navigation.NavigateTo("/LogIn");
+        }
+    }
+}
